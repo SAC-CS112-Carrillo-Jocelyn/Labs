4898a75bbb5e283de5596e712242ae1f505d82da
Jocelyn Carrillo <jocelyncarrillo94@gmail.com>
2015-09-30 21:41:27 -0700

    labs

--- InClassLabs/src/week6/W6L3b.java
+++ InClassLabs/src/week6/W6L3b.java
@@ -1,10 +1,15 @@
 package week6;
+
 import java.util.Scanner;
-public class W6L3a_GuessNum {
+
+public class W6L3b {
 
 	public static void main(String[] args) {
 		int rand, userNum;
 		int min=1, max=10;
+		String tBig="Sorry, the number you guessed is too big. ";
+		String tSmall="Sorry, the number you guessed is too small. ";
+		String uString;
 		Scanner input = new Scanner(System.in);
 				
 		System.out.print("I have a number inbetween 1 & 10, "
@@ -12,20 +17,20 @@ public class W6L3a_GuessNum {
 		rand = min+(int)(Math.random()*max);
 		//System.out.print(rand);
 		userNum=input.nextInt();
-		if(userNum > max || userNum < min)
-			System.out.printf("The number %d is out of the valid range!", userNum);
+		if(userNum > max || userNum < min){
+			System.out.printf("The number %d is out of the valid range!", 
+					userNum);
+		}
 		else{
-			if(userNum > rand)
-				System.out.printf("Sorry, the number you guessed is too big."
-						+ "\nThe correct number was %d.", rand);
-			else if(userNum < rand)
-				System.out.printf("Sorry, the number you guessed is too small."
-						+ "\nThe correct number was %d.",rand);
-			else
+			if(userNum==rand)
 				System.out.printf("Congratz, you guessed correctly!");
+			else{
+				uString =(userNum > rand)? tBig:tSmall;
+				System.out.printf("%s\nThe correct number was %d.", 
+						uString ,rand);
+			}			
 		}
 			input.close();
-
 	}
-
 }
+
