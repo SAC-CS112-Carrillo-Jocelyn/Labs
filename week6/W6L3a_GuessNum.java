f09c7f81290c3e282ba3f47a74547cf6ec8f6019
Jocelyn Carrillo <jocelyncarrillo94@gmail.com>
2015-09-30 20:55:02 -0700

    W6L3A

--- week6/W6L3a_GuessNum.java
+++ week6/W6L3a_GuessNum.java
@@ -5,17 +5,24 @@ public class W6L3a_GuessNum {
 	public static void main(String[] args) {
 		int rand;
 		int userNum;
+		int min =1, max =10;
 		Scanner input = new Scanner(System.in);
 				
 		System.out.print("I have a number inbetween 1 & 10, can you guess what it is? ");
-		rand = 1+(int)(Math.random()*10);
+		rand = min+(int)(Math.random()*max);
 		//System.out.print(rand);
 		userNum=input.nextInt();
-		if(userNum > rand || userNum < rand)
-			System.out.printf("Sorry, the number you guessed is wrong.\nThe correct number was %d.", rand);
-		else
-			System.out.printf("Congratz, you guessed correctly!");
-		input.close();
+		if(userNum > max || userNum < min)
+			System.out.printf("The number %d is out of the valid range!", userNum);
+		else{
+			if(userNum > rand)
+				System.out.printf("Sorry, the number you guessed is too big.\nThe correct number was %d.", rand);
+			else if(userNum < rand)
+				System.out.printf("Sorry, the number you guessed is too small.\nThe correct number was %d.",rand);
+			else
+				System.out.printf("Congratz, you guessed correctly!");
+		}
+			input.close();
 
 	}
 
