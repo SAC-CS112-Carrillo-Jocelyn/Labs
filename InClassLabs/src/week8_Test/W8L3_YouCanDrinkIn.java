8066e0387565218b185d84c65c33450699fa1173
Jocelyn Carrillo <jocelyncarrillo94@gmail.com>
2015-10-14 19:44:51 -0700

    stuff

---
+++ InClassLabs/src/week8_Test/W8L3_YouCanDrinkIn.java
@@ -0,0 +1,24 @@
+package week8_Test;
+
+import javax.swing.JOptionPane;
+
+public class W8L3_YouCanDrinkIn {
+
+	public static void main(String[] args) {
+		int age, drinkingAge= 21, canDrinkIn;
+		
+		age=Integer.parseInt(JOptionPane.showInputDialog("What year where you born in?"));
+		age=2015-age;
+		if(drinkingAge>=age){
+			canDrinkIn=drinkingAge-age;
+			JOptionPane.showMessageDialog(null, "You are not of drinking age."
+					+ " You'll be able to drink in "+canDrinkIn+" years.");
+		}
+		else
+			JOptionPane.showMessageDialog(null,"You can drink!");
+
+	}
+
+	
+
+}
