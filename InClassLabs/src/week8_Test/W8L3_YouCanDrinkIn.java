6a3e2d7fbbff4c015d9fb6d9637b5f81882545fe
Jocelyn Carrillo <jocelyncarrillo94@gmail.com>
2015-10-14 19:45:29 -0700

    stuff

--- InClassLabs/src/week8_Test/W8L3_YouCanDrinkIn.java
+++ InClassLabs/src/week8_Test/W8L3_YouCanDrinkIn.java
@@ -9,7 +9,9 @@ public class W8L3_YouCanDrinkIn {
 		
 		age=Integer.parseInt(JOptionPane.showInputDialog("What year where you born in?"));
 		age=2015-age;
+		
 		if(drinkingAge>=age){
+			
 			canDrinkIn=drinkingAge-age;
 			JOptionPane.showMessageDialog(null, "You are not of drinking age."
 					+ " You'll be able to drink in "+canDrinkIn+" years.");
