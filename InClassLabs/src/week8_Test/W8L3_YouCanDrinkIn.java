eb4a40c5b77d5dadff0d23e810f84726c4ee8bd8
Jocelyn Carrillo <jocelyncarrillo94@gmail.com>
2015-10-14 19:47:53 -0700

    s

--- InClassLabs/src/week8_Test/W8L3_YouCanDrinkIn.java
+++ InClassLabs/src/week8_Test/W8L3_YouCanDrinkIn.java
@@ -1,10 +1,8 @@
 package week8_Test;
-
 import javax.swing.JOptionPane;
-
 public class W8L3_YouCanDrinkIn {
-
 	public static void main(String[] args) {
+		
 		int age, drinkingAge= 21, canDrinkIn;
 		
 		age=Integer.parseInt(JOptionPane.showInputDialog("What year where you born in?"));
