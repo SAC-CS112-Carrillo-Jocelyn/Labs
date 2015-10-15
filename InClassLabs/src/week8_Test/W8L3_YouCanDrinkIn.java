da2d84f776b92150218bfa8543dfd96cc49d711f
Jocelyn Carrillo <jocelyncarrillo94@gmail.com>
2015-10-14 19:54:44 -0700

    drink

--- InClassLabs/src/week8_Test/W8L3_YouCanDrinkIn.java
+++ InClassLabs/src/week8_Test/W8L3_YouCanDrinkIn.java
@@ -2,7 +2,8 @@ package week8_Test;
 import javax.swing.JOptionPane;
 public class W8L3_YouCanDrinkIn {
 	public static void main(String[] args) {
-		
+		//Can user drink?
+		//If not tell em how many more years they'll have to wait till they can.
 		int age, drinkingAge= 21, canDrinkIn;
 		
 		age=Integer.parseInt(JOptionPane.showInputDialog("What year where you born in?"));
