23717ce26801f0b5c2c9c0ce4adbd0ec77215529
Jocelyn Carrillo <jocelyncarrillo94@gmail.com>
2015-10-14 20:00:41 -0700

    u

---
+++ InClassLabs/src/week8_Test/W8L4_NameBirth_3Times.java
@@ -0,0 +1,22 @@
+package week8_Test;
+
+import javax.swing.JOptionPane;
+
+public class W8L4_NameBirth_3Times {
+
+	public static void main(String[] args) {
+		//Output user name & age 3 TIMES
+		String name;
+		int birthYear, age;
+		
+		for(int i=1; i<=3; i++){
+			name = JOptionPane.showInputDialog("What is your name?");
+			birthYear=Integer.parseInt(JOptionPane.showInputDialog("What year where you born in?"));
+			
+			age=2015-birthYear;
+			String outputString=String.format("Hello, %s, you're %d years old today!", name,age);
+			JOptionPane.showMessageDialog(null,outputString);
+		}
+	}
+
+}
