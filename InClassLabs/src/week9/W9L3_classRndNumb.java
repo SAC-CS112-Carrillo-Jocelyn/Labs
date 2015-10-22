b1b8a15c37ea5f528f76cb28fc26d0f02698db56
Jocelyn Carrillo <jocelyncarrillo94@gmail.com>
2015-10-21 21:21:25 -0700

    s

--- InClassLabs/src/week9/W9L3_classRndNumb.java
+++ InClassLabs/src/week9/W9L3_classRndNumb.java
@@ -1,7 +1,4 @@
 package week9;
-
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 
 public class W9L3_classRndNumb {
@@ -12,17 +9,20 @@ public class W9L3_classRndNumb {
 	}
 
 	public static void main(String[] args) {
+		//Declare
 		int diceNumb, remainder,d=-1;
 		String oddOrEven;
-		Scanner input=new Scanner(System.in);
 		
+		//Start
 		JOptionPane.showMessageDialog(null, "Throw a dice!");
+		
 		diceNumb= NumberGenerator();
-		oddOrEven=(JOptionPane.showInputDialog(null, "Do you think the number rolled is odd or even?").toLowerCase());
+		
+		oddOrEven=(JOptionPane.showInputDialog
+				(null, "Do you think the number rolled is odd or even?").toLowerCase());
 
 		//a odd # will have a remainder of 1, while a even # will have a remainder of 0
 		remainder = diceNumb%2; 
-		System.out.print(diceNumb +" "+ remainder);//Debug
 		
 		if(oddOrEven.equals("odd")||oddOrEven.equals("even")){
 			switch(oddOrEven){
